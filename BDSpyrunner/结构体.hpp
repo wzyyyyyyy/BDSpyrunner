6adc1b684e8de326196541b03c755031978fc559
twoone-3 <62778573+twoone-3@users.noreply.github.com>
2020-09-26 16:16:59 +0800

    更新

--- BDSpyrunner/结构体.hpp
+++ BDSpyrunner/结构体.hpp
@@ -31,10 +31,37 @@ struct Block {
 struct BlockPos {
 	int x, y, z;
 };
+struct BlockActor {
+	// 取方块
+	Block* getBlock() {
+		return *reinterpret_cast<Block**>(reinterpret_cast<VA>(this) + 16);
+	}
+	// 取方块位置
+	BlockPos* getPosition() {				// IDA BlockActor::BlockActor
+		return reinterpret_cast<BlockPos*>(reinterpret_cast<VA>(this) + 44);
+	}
+};
 struct BlockSource {
 	Block* getBlock(BlockPos* bp) {
 		return SYMCALL(Block*, "?getBlock@BlockSource@@QEBAAEBVBlock@@AEBVBlockPos@@@Z",
 			this, bp);
+	}
+};
+struct Dimension {
+	// 获取方块源
+	VA getBlockSouce() {					// IDA Level::tickEntities
+		return *((VA*)this + 9);
+	}
+};
+struct Level {
+	// 获取维度
+	Dimension* getDimension(int did) {
+		return SYMCALL(Dimension*, "?getDimension@Level@@QEBAPEAVDimension@@V?$AutomaticID@VDimension@@H@@@Z",
+			this, did);
+	}
+	// 获取计分板
+	VA getScoreBoard() {				// IDA Level::removeEntityReferences
+		return *(VA*)((VA)this + 8280);
 	}
 };
 struct Actor {
@@ -55,10 +82,35 @@ struct Actor {
 		return SYMCALL(Vec3*, "?getPos@Actor@@UEBAAEBVVec3@@XZ",
 			this);
 	}
+	// 是否悬空
+	const BYTE isStand() {				// IDA MovePlayerPacket::MovePlayerPacket
+		return *reinterpret_cast<BYTE*>(reinterpret_cast<VA>(this) + 416);
+	}
 
 };
 struct Mob : Actor {
+	// 获取状态列表
+	//std::vector<MobEffectInstance>* getEffects() {					// IDA Mob::addAdditionalSaveData
+	//	return (std::vector<MobEffectInstance>*)((VA*)this + 152);
+	//}
 
+	// 获取装备容器
+	VA getArmor() {					// IDA Mob::addAdditionalSaveData
+		return VA(this) + 1400;
+	}
+	// 获取手头容器
+	VA getHands() {
+		return VA(this) + 1408;		// IDA Mob::readAdditionalSaveData
+	}
+	// 保存当前副手至容器
+	VA saveOffhand(VA hlist) {
+		return SYMCALL(VA, "?saveOffhand@Mob@@IEBA?AV?$unique_ptr@VListTag@@U?$default_delete@VListTag@@@std@@@std@@XZ",
+			this, hlist);
+	}
+	// 获取地图信息
+	VA getLevel() {					// IDA Mob::die
+		return *((VA*)((VA)this + 816));
+	}
 };
 struct Player : Mob {
 	// 发送数据包
@@ -148,6 +200,80 @@ struct ItemStack : ItemStackBase {
 			MSSYM_B1QA6isNullB1AE13ItemStackBaseB2AAA4QEBAB1UA3NXZ,
 			this);
 	}*/
+};
+struct LevelContainerModel {
+	// 取开容者
+	Player* getPlayer() {
+		return ((Player**)this)[26];
+	}
+};
+
+
+struct Container {
+	VA vtable;
+	// 获取容器内所有物品
+	VA getSlots(std::vector<ItemStack*>* s) {
+		return SYMCALL(VA, "?getSlots@Container@@UEBA?BV?$vector@PEBVItemStack@@V?$allocator@PEBVItemStack@@@std@@@std@@XZ",
+			this, s);
+	}
+
+};
+
+struct SimpleContainer : Container {
+	// 获取一个指定框内物品
+	ItemStack* getItem(int slot) {
+		return SYMCALL(ItemStack*, "?getItem@SimpleContainer@@UEBAAEBVItemStack@@H@Z", this, slot);
+	}
+	// 设置一个指定框内的物品
+	VA setItem(int slot, ItemStack* item) {
+		return SYMCALL(VA, "?setItem@SimpleContainer@@UEAAXHAEBVItemStack@@@Z",
+			this, slot, item);
+	}
+};
+
+struct FillingContainer : Container {
+	// 格式化容器所有物品至tag
+	VA save(VA tag) {
+		return SYMCALL(VA, "?save@FillingContainer@@QEAA?AV?$unique_ptr@VListTag@@U?$default_delete@VListTag@@@std@@@std@@XZ",
+			this, tag);
+	}
+	// 设置容器中指定位置的物品
+	VA setItem(int i, VA item) {
+		return SYMCALL(VA, "?setItem@FillingContainer@@UEAAXHAEBVItemStack@@@Z",
+			this, i, item);
+	}
+};
+
+struct IContainerManager {
+	VA vtable;
+};
+
+struct PlayerInventoryProxy {
+	VA vtable;
+	VA vtable2;
+	int mSelected;
+	char uk1[4];
+	ItemStack mInfiniteItem;
+	char mSelectedContainerId;
+	char uk2[7];
+	VA mInventory;
+	std::vector<ItemStack> mComplexItems;
+	std::weak_ptr<VA> mHudContainerManager;
+};
+
+struct ContainerItemStack
+	:ItemStack {
+
+};
+
+struct ContainerManagerModel {
+	// 取开容者
+	Player* getPlayer() {				// IDA ContainerManagerModel::ContainerManagerModel
+		return *reinterpret_cast<Player**>(reinterpret_cast<VA>(this) + 8);
+	}
+};
+struct LevelContainerManagerModel
+	:ContainerManagerModel {
 };
 struct TextPacket {
 	char filler[0xC8];
